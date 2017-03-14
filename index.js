a4f7d6cc84615d398025ffceba9a839edcea0fae
ANyanCatFan <saitam.spatz@gmail.com>
2017-03-14 20:31:01 +0100

    You can search images with a specific name

--- index.js
+++ index.js
@@ -5,19 +5,35 @@ firebase.initializeApp(require("./config"));
 const ref = firebase.database().ref("images");
 
 io.sockets.on("connection", function(socket) {
-  socket.on("push", function(clientData) {
+  socket.on("push", function(inData) {
     ref.push({
-      name: clientData.name,
-      image: clientData.image,
+      name: inData.name,
+      image: inData.image,
       time: new Date().getTime()
     });
-    ref.on("value", function(databaseData) {
-      for (var i in databaseData.val()) {
-        if (databaseData.val()[i].name == clientData.name) {
+    ref.on("value", function(dbData) {
+      for (var i in dbData.val()) {
+        if (dbData.val()[i].name == inData.name) {
           socket.emit("feedback", {
-            image: databaseData.val()[i].image
+            image: dbData.val()[i].image
           });
         }
+      }
+    });
+  });
+
+  socket.on("search", function(inData) {
+    ref.on("value", function(dbData) {
+      var outData = [];
+      for (var i in dbData.val()) {
+        if (dbData.val()[i].name == inData.name) {
+          outData.push(dbData.val()[i].image);
+        }
+      }
+      if (outData) {
+        socket.emit("feedback", {
+          images: outData
+        });
       }
     });
   });
